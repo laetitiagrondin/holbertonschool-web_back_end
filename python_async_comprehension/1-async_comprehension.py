9631edc2687c095e6e079409cb32c1b3c15f0a0a
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-04-07 14:20:21 +0200

    Commit

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> list:
+    return [i async for i in async_generator()]
