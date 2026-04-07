0666419569ab69d410cd3a41600c3bd1b30d3190
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-04-07 11:24:58 +0200

    Commit

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,6 @@
 #!/usr/bin/env python3
+from typing import Callable
+def make_multiplier(multiplier:float) -> Callable[[float], float]:
+    def multiply(n: float) -> float:
+        return n * multiplier
+    return multiply
