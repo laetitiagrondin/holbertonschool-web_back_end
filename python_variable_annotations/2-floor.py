d6738ae69ca19cb519350483e4458853bd2fde36
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-04-10 09:41:38 +0200

    Documentation + Pycodestyle

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""Basic annotations - floor"""
 import math
-def floor(n:float) -> int:
+
+
+def floor(n: float) -> int:
+    """Takes a float n as argument and returns the floor of the float."""
     return math.floor(n)
