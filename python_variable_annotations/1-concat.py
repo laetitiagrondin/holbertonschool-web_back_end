1aaa8fa29deba4b8d26eeb41ee90e0b2e71130d7
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-04-10 09:36:51 +0200

    Pycodestyle

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 """Basic annotations - concat"""
 
 
-def concat(str1:str, str2:str) -> str:
+def concat(str1: str, str2: str) -> str:
     """Concat string str1 and string str2 and returns a concatenated string."""
     return str1 + str2
