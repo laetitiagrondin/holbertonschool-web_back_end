2982ccd573796562f54b442762544e53ade4c93e
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-04-10 09:59:29 +0200

    Documentation

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""Complex types - string and int/float to tuple"""
 from typing import Iterable, Sequence, List, Tuple
+
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a tuple of the string k and the square
+       of the int/float v as a float."""
     return [(i, len(i)) for i in lst]
