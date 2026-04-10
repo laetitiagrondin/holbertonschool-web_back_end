698c9e14b25465f606c59d512f5d808719de6d15
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-04-10 10:03:28 +0200

    Documentation + Pycodestyle

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+"""Complex types - functions"""
 from typing import Callable
-def make_multiplier(multiplier:float) -> Callable[[float], float]:
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Takes a float multiplier as argument and returns a function
+       that multiplies a float by multiplier."""
     def multiply(n: float) -> float:
         return n * multiplier
     return multiply
