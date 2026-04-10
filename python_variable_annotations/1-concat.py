3fec8f0023e00e7121525f1b3e5b571c85c92b2d
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-04-10 09:35:46 +0200

    Documentation

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
+"""Basic annotations - concat"""
+
+
 def concat(str1:str, str2:str) -> str:
+    """Concat string str1 and string str2 and returns a concatenated string."""
     return str1 + str2
