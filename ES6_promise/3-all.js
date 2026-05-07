3036184b03623c34452b7daae76694baac4e86f2
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-05-07 13:40:27 +0200

    Correction task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,12 +2,8 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
     return Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-        console.log(
-            photoResponse.body,
-            userResponse.firstName,
-            userResponse.lastName
-        );
+    .then((results) => {
+        console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
     .catch(() => {
         console.log('Signup system offline');
