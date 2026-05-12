c7521e7250d0fed70311e6ff12b043c8ce958d4c
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-05-12 11:17:35 +0200

    Task 1 + README.md

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+console.log('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', data => {
+    process.stdout.write(`Your name is: ${data}`);
+});
+
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+});
