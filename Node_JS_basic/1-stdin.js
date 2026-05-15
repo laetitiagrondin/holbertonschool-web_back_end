0efef5ba075b7b7b5fd0544a54ebedca6b89276a
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-05-15 13:56:20 +0200

    Verified code using ESLint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,10 +3,10 @@ console.log('Welcome to Holberton School, what is your name?\n');
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', data => {
-    process.stdout.write(`Your name is: ${data}`);
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,24 +2,24 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-    if (req.url === '/') {
-        res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-        res.write('This is the list of our students\n');
-        countStudents(process.argv[2])
-        .then((data) => {
-            res.end(data);
-        })
-        .catch((err) => {
-            res.end(err.message);
-        });
-    } else {
-        res.statusCode = 404;
-        res.end('Not found');
-    }
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.end(data);
+    })
+      .catch((err) => {
+        res.end(err.message);
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
+  }
 });
 
 app.listen(1245);
