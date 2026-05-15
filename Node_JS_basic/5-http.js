27c70e82e29992302cf996e78f5748291c4f3ec0
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-05-15 18:01:02 +0200

    Correction

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,32 +2,25 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-    const database = process.argv[2];
-
   if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-    return;
-  }
-
-  if (req.url === '/students') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('This is the list of our students\n');
-    countStudents(database)
-      .then(() => {
-        res.end();
+  } else if (req.url === '/students') {
+    countStudents(process.argv[2])
+      .then((data) => {
+        const responseText = `This is the list of our students\n${data}`;
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(responseText);
       })
-      .catch(() => {
-        res.end('Cannot load the database');
+      .catch((err) => {
+        const responseText = `This is the list of our students\n${err.message}`;
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(responseText);
       });
-    return;
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
   }
-
-  res.statusCode = 404;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Not found');
 });
 
 app.listen(1245);
