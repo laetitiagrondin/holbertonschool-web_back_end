b8d7772f43a95563e30bb5f65ca453d557bb1fb4
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-05-15 15:59:53 +0200

    Correction

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -11,11 +11,18 @@ app.get('/', (req, res) => {
 app.get('/students', (req, res) => {
   res.type('text/plain');
   res.write('This is the list of our students\n');
+  const originalLog = console.log;
+  const logs = [];
+  console.log = (message) => {
+    logs.push(message);
+  };
   countStudents(process.argv[2])
     .then(() => {
-      res.end();
+      console.log = originalLog;
+      res.end(logs.join('\n'));
     })
     .catch(() => {
+      console.log = originalLog;
       res.end('Cannot load the database');
     });
 });
