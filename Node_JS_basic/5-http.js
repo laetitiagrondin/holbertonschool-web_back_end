898dcf864f96bd5961636618da0090ad054ace97
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-05-15 14:15:59 +0200

    Correction fo the code

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,22 +2,22 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
+  if (req.url === '/') {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
+  res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
     res.write('This is the list of our students\n');
     countStudents(process.argv[2])
-      .then((data) => {
-        res.end(data);
-      })
-      .catch((err) => {
-        res.end(err.message);
+      .then(() => {})
+      .catch(() => {
+        res.end('Cannot load the database');
       });
   } else {
     res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Not found');
   }
 });
