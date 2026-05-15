59e6723f1aee104dd358cbdcdde44e65fc0425b4
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-05-15 14:21:20 +0200

    Correction of indentation

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,9 +3,9 @@ const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
