89f2ba20286fb33cbe67c03ba51a63210dedd354
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-05-15 15:45:57 +0200

    Fixed errors

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,18 +4,19 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.get('/', (req, res) => {
+  res.set('text/plain');
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
+  res.set('text/plain');
+  res.write('This is the list of our students\n');
   countStudents(process.argv[2])
-    .then((data) => {
-      const responseText = `This is the list of our students\n${data}`;
-      res.send(responseText);
+    .then(() => {
+      res.end();
     })
-    .catch((err) => {
-      const responseText = `This is the list of our students\n${err.message}`;
-      res.send(responseText);
+    .catch(() => {
+      res.end('Cannot load the database');
     });
 });
 
